720d3913862c6e88cade6c0f844cefbdcf36c5d1
mfrancis95 <mikefrancis95@gmail.com>
2016-02-23 09:14:30 -0500

    Initial commit

---
+++ blocker.js
@@ -0,0 +1,19 @@
+var exec = require("child_process").exec;
+
+var regex;
+
+function middleware(request, response, next) {
+    if (request.path.search(regex) >= 0) {
+        var ip = request.ip;
+        ip = ip.slice(ip.lastIndexOf(":") + 1);
+        exec("iptables -A INPUT -s " + ip + " -j DROP");
+    }
+    else {
+        next();
+    }
+}
+
+module.exports = pattern => {
+    regex = pattern;
+    return middleware;
+};
