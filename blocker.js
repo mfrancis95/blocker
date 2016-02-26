f818958ac534765d055f25f5e06d03a49687492d
mfrancis95 <mikefrancis95@gmail.com>
2016-02-26 17:43:04 -0500

    Add on block callback argument and logger

--- blocker.js
+++ blocker.js
@@ -1,11 +1,18 @@
 var exec = require("child_process").exec;
 
-var regex;
+var regex, callback;
+
+function logger(request) {
+    console.log(`Blocking ${request.ip} for ${request.path}`);
+}
 
 function middleware(request, response, next) {
     if (request.path.search(regex) >= 0) {
         var ip = request.ip;
         ip = ip.slice(ip.lastIndexOf(":") + 1);
+        if (callback) {
+            callback(request);
+        }
         exec(`iptables -A INPUT -s ${ip} -j DROP`);
     }
     else {
@@ -13,7 +20,9 @@ function middleware(request, response, next) {
     }
 }
 
-module.exports = pattern => {
+module.exports = (pattern, blockCallback) => {
     regex = pattern;
+    callback = blockCallback;
     return middleware;
 };
+module.exports.logger = logger;
