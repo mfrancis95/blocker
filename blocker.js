4227c45fcccb3475baffe29ae775e746e488211c
mfrancis95 <mikefrancis95@gmail.com>
2016-03-15 01:08:41 -0400

    Move most code over to predicate repo

--- blocker.js
+++ blocker.js
@@ -1,30 +1,14 @@
 var exec = require("child_process").exec;
 
-var predicate, callback;
-
-function middleware(request, response, next) {
-    if (predicate(request)) {
-        if (callback) {
-            callback(request);
+function blocker(logger) {
+    return request => {
+        if (logger) {
+            logger(request);
         }
         var ip = request.ip;
         exec(`iptables -A INPUT -s ${ip.slice(ip.lastIndexOf(":") + 1)} -j DROP`);
-    }
-    else {
-        next();
-    }
+    };
 }
 
-function regexToPredicate(regex) {
-    return request => request.path.search(regex) >= 0;
-}
-
-module.exports = (blockPredicate, blockCallback) => {
-    if (blockPredicate instanceof RegExp) {
-        blockPredicate = regexToPredicate(blockPredicate);
-    }
-    predicate = blockPredicate;
-    callback = blockCallback;
-    return middleware;
-};
+module.exports = logger => blocker(logger);
 module.exports.logger = request => console.log(`Blocking ${request.ip} for ${request.path}`);
